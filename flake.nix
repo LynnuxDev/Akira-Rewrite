dd891d8697c2a0fcd384361008aebe83d4731b4b
Dark-LYNN <botdevlynn@gmailcom>
2025-05-14 15:07:52 +0200

    flake

--- flake.nix
+++ flake.nix
@@ -7,29 +7,29 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
 
-      in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.nodejs_24
-            pkgs.prisma-engines
-            pkgs.pnpm
-            pkgs.direnv
-            pkgs.openssl
-          ];
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs_24
+          pkgs.prisma-engines
+          pkgs.pnpm
+          pkgs.direnv
+          pkgs.openssl
+        ];
 
-          shellHook = ''
-            export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
-            export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
-            export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
-            export PRISMA_ENGINES_CHECKSUM_IGNORE_MISSING="1"
-          '';
-        };
-      }
-    );
+        shellHook = ''
+          export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
+          export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
+          export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
+          export PRISMA_ENGINES_CHECKSUM_IGNORE_MISSING="1"
+        '';
+      };
+    }
+  );
 }
